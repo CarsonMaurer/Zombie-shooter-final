305808436acfe492b82f9019ebaad6247c1b3957
Carson Maurer <carsonjmaurer@gmail.com>
2023-04-24 11:32:09 -0700

    added shooting

---
+++ zombie shooter/Assets/Scripts/GunController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform gunBarrel;
+    public float bulletSpeed = 20f;
+    public float cooldownTime = 0.5f; // Time in seconds between shots
+    public float bulletLifetime = 3f; // Time in seconds before a bullet is destroyed
+
+    private Transform playerTransform;
+    private float lastShotTime = Mathf.NegativeInfinity;
+
+    private void Start()
+    {
+        playerTransform = transform.parent; // assuming the gun is a child of the player object
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > lastShotTime + cooldownTime)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        lastShotTime = Time.time;
+
+        GameObject bullet = Instantiate(bulletPrefab, gunBarrel.position, Quaternion.identity);
+        Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = playerTransform.forward * bulletSpeed;
+        bulletRigidbody.useGravity = false;
+
+        // Rotate the bullet to match the gun barrel's rotation
+        bullet.transform.rotation = gunBarrel.rotation;
+
+        
+    }
+}
