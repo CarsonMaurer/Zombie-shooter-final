f18362cbad54e3f46d9930e7d271fe211d058a2a
Carson Maurer <carsonjmaurer@gmail.com>
2023-05-05 11:30:31 -0700

    fixed enemy going through object

--- zombie shooter/Assets/Scripts/GunController.cs
+++ zombie shooter/Assets/Scripts/GunController.cs
@@ -5,15 +5,16 @@ public class GunController : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform gunBarrel;
     public float bulletSpeed = 20f;
-    public float cooldownTime = 0.5f; // Time in seconds between shots
-    public float bulletLifetime = 3f; // Time in seconds before a bullet is destroyed
+    public float cooldownTime = 0.5f; 
+    public float bulletLifetime = 3f; 
+    public AnimationClip shootingAnimationClip;
 
     private Transform playerTransform;
     private float lastShotTime = Mathf.NegativeInfinity;
 
     private void Start()
     {
-        playerTransform = transform.parent; // assuming the gun is a child of the player object
+        playerTransform = transform.parent; 
     }
 
     private void Update()
@@ -28,7 +29,7 @@ private void Shoot()
 {
     lastShotTime = Time.time;
 
-    // Spawn the initial bullet
+   
     Vector3 spawnPosition = gunBarrel.position + gunBarrel.forward * 0.5f;
 
     GameObject bullet = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity);
@@ -36,12 +37,12 @@ private void Shoot()
     bulletRigidbody.velocity = playerTransform.forward * bulletSpeed;
     bulletRigidbody.useGravity = false;
 
-    // Rotate the bullet to match the gun barrel's rotation
+    
     bullet.transform.rotation = gunBarrel.rotation * Quaternion.Euler(90f, 0f, 0f);
 
     Destroy(bullet, bulletLifetime);
 
-    // Spawn two additional bullets with a rotation of +/- 35 degrees around the y-axis and a z-rotation of -120 and 120
+   
     for (int i = -1; i <= 1; i += 2)
     {
         Quaternion rotation = Quaternion.Euler(0f, i * 25f, 0f);
@@ -51,15 +52,18 @@ private void Shoot()
         bulletCloneRigidbody.velocity = (rotation * playerTransform.forward) * bulletSpeed;
         bulletCloneRigidbody.useGravity = false;
 
-        // Rotate the bullet to match the gun barrel's rotation
+       
         bulletClone.transform.rotation = gunBarrel.rotation * Quaternion.Euler(90f, 0f, 0f);
+         
+    
 
-        // Disable collisions between the bullets
+        
         Physics.IgnoreCollision(bullet.GetComponent<Collider>(), bulletClone.GetComponent<Collider>());
 
         Destroy(bulletClone, bulletLifetime);
     }
-} 
+   
+}
 
 
 
