e42a166759bc0177ca6e638e9faa4c5fe9bae9fc
Carson Maurer <carsonjmaurer@gmail.com>
2023-05-11 11:03:58 -0700

    bugs

--- zombie shooter/Assets/Scripts/FollowPlayer.cs
+++ zombie shooter/Assets/Scripts/FollowPlayer.cs
@@ -4,17 +4,24 @@ public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
     public float speed = 5f;
+    public float followRange = 35f; // Maximum distance for following
     
     void Update()
     {
         // Calculate the direction to the player
         Vector3 direction = player.position - transform.position;
-        direction.Normalize();
+        float distanceToPlayer = direction.magnitude;
         
-        // Turn to face the player
-        transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
-        
-        // Move towards the player
-        transform.position += direction * speed * Time.deltaTime;
+        // Check if the player is within range
+        if (distanceToPlayer <= followRange)
+        {
+            direction.Normalize();
+            
+            // Turn to face the player
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+            
+            // Move towards the player
+            transform.position += direction * speed * Time.deltaTime;
+        }
     }
 }
